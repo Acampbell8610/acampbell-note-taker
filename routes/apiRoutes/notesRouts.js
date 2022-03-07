3f50d180b95e4cc5d70890d6bf23e4abaa755458
acamp <acampbell8610@gmail.com>
2022-03-06 19:12:26 -0600

    added routes

--- routes/apiRoutes/notesRouts.js
+++ routes/apiRoutes/notesRouts.js
@@ -1,36 +1,23 @@
-const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
-const { animals } = require('../../data/animals');
-
 const router = require('express').Router();
+ const { newNote } = require('../../lib/notes');
+ const notes = require("../../db/db.json");
 
-router.get('/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
+const uuid = require('../../helpers/helper')
 
-router.get('/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
 
-router.post('/animals', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
+router.get('/notes', (req, res) => {
+  console.log('here are the notes', notes)
+    res.json(notes);
+  });
 
-    // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
-});
+router.post('/notes', (req, res) => {
+  req.body.id = uuid();
 
-module.exports  = router;
+  /// adds id to the new note
+  const note = newNote(req.body, notes);
+  res.json(note)
+})
+
+
+  
+module.exports = router;
